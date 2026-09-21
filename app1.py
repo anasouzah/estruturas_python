e2eafe0b4ff671e396dbafda3688943d0a2c7b18
anasouzah <anaestudoos1@gmail.com>
2026-09-21 12:55:24 -0300

    app ok

---
+++ app1.py
@@ -0,0 +1,14 @@
+# recebe o nome do usário na vriavél
+name = input("Qual o seu nome?")
+
+# receba a idade do usuário na variável
+age = input("Qual a sua idade?")  
+
+# Exibe uma mensagem formatada
+print()
+print("Olá", name, "!")
+print("Você tem", age, "anos")
+
+# Usando esting formatada (f) - melhor prática
+print()
+print(f"Olá {name}! Você tem {age} anos.")
