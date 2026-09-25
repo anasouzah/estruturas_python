d098294556ca29d35dd7dc815363ce7e7d591f43
anasouzah <anaestudoos1@gmail.com>
2026-09-25 12:50:21 -0300

    ok

---
+++ app3.py
@@ -0,0 +1,16 @@
+age = 22
+has_invitation = False
+is_vip = True
+
+if age >= 18 and has_invitation:
+    print("Entrada permitida") 
+
+elif is_vip:
+    print('Que bom ver você novamente')    
+
+else: 
+    print("Entrada não permitida") 
+
+print("Acabou")
+
+
